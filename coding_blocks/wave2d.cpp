96f52297b9f0de5859b111bc3d5a17faf490986b
Sahil Siddiqui <ssiddiqui946@gmail.com>
2021-02-08 15:07:25 +0530

    coding block

---
+++ coding_blocks/wave2d.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a[1000][1000]={0};
+    int m,n;
+    cin>>n>>m;
+    int val=1;
+    for(int row=0;row<=n-1;row++){
+        for(int col=0;col<=m-1;col++){
+            a[row][col]=val;
+            val=val+1;
+            cout<<a[row][col]<<" ";
+        }
+        cout<<endl;
+    }
+
+    for(int col=0;col<m;col++){
+        //if col is even
+        if(col%2==0){
+            for(int row=0;row<=n-1;row++){
+                cout<<a[row][col]<<" ";
+            }
+            cout<<endl;
+        }
+        //if col is odd
+        else{
+            for(int row=n-1;row>=0;row--){
+                cout<<a[row][col]<<" ";
+            }
+            cout<<endl;
+        }
+    return 0;
+}
